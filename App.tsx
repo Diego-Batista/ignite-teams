764a6bb65959807a95f325a697ab2c6e27dad374
Diego-Batista <diegobtistadev@gmail.com>
2023-11-27 09:22:01 -0300

    Defining the application theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components/native";
 
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+        <Groups />
+    </ThemeProvider>
+
   );
 }
 
