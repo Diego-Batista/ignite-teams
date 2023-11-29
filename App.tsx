47062d547f71f6bed64748b8a0c82511cba22eb9
Diego-Batista <diegobtistadev@gmail.com>
2023-11-29 09:55:20 -0300

    Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-        <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}    
     </ThemeProvider>
 
   );
