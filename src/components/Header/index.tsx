782cb9494f1b484694440b561d2148bb556e11a6
Diego-Batista <diegobtistadev@gmail.com>
2023-11-29 13:10:37 -0300

    Creating Highlight Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import LogoImage from '@assets/logo.png';
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-export function Header() {
+type HeaderProps = {
+    showBackButton?: boolean
+}
+
+export function Header({ showBackButton }: HeaderProps) {
     return (
         <Container>
-            <BackButton>
-                <BackIcon />
-            </BackButton>
+            {showBackButton &&
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
+            
             <Logo source={LogoImage}/>
         </Container>
     )
