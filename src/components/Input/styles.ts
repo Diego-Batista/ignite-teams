680cfb1f42cf297f02e68c2b98a7fe8dd20faa03
Diego-Batista <diegobtistadev@gmail.com>
2023-12-02 10:01:29 -0300

    Using CSS Helper

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
     flex: 1;
@@ -14,8 +14,11 @@ export const Container = styled(TextInput)`
 
     padding: 16px;
 
-    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-    color: ${({ theme }) => theme.COLORS.WHITE}
+    ${({ theme }) => css`
+        background-color: ${ theme.COLORS.GRAY_700};
+        font-family: ${ theme.FONT_FAMILY.REGULAR};
+        font-size: ${ theme.FONT_SIZE.MD}px;
+        color: ${ theme.COLORS.WHITE}
+    `}
+
 `
