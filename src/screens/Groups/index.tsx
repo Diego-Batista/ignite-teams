4b332367ea5073a8ca3c55c4403adc1505be8ba8
Diego-Batista <diegobtistadev@gmail.com>
2023-12-14 18:18:05 -0300

    Loading Refactor

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -27,11 +27,12 @@ export function Groups() {
       const data = await groupGetAll()
       setGroup(data);
 
-      setIsLoading(false);
     } catch (error) {
       Alert.alert('Turmas', 'Não foi possível carregar as turmas');
       console.log(error)
-    }
+    } finally {
+      setIsLoading(false);
+    } 
   }
 
   function handleOpenGroup(group: string) {
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -66,13 +66,17 @@ export function Players() {
       async function fetchPlayersByTeam() {
         try {
           setIsLoading(true);
+
           const playersByTeam = await playersGetByGroupAndTeam(group, team);
+
           setPlayers(playersByTeam);
-          setIsLoading(false);
         } catch (error) {
           console.log(error);
+          
           Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
-        }
+        } finally {
+          setIsLoading(false);
+        } 
       }
 
       async function handlePlayerRemove(playerName: string) {
