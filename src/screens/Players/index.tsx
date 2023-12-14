6dcbef0899cc9607d1eeebdb098382008ab5464a
Diego-Batista <diegobtistadev@gmail.com>
2023-12-14 16:05:50 -0300

    Remove Group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,7 +6,8 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { ListEmpty } from "@components/ListEmpty";
 import { PlayerCard } from "@components/PlayerCard";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { groupRemoveByName } from "@storage/group/groupRemoveByName";
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup";
@@ -26,6 +27,8 @@ export function Players() {
     const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
     const route = useRoute();
+
+    const navigation = useNavigation();
 
     const { group } = route.params as RouteParams;
 
@@ -79,6 +82,28 @@ export function Players() {
     
           Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.');
         }
+      }
+
+      async function groupRemove() {
+        try {
+          await groupRemoveByName(group);
+          navigation.navigate('groups');
+    
+        } catch (error) {
+          console.log(error);
+          Alert.alert('Remover Grupo', 'Não foi posível remover o grupo');
+        }
+      }
+    
+      async function handleGroupRemove() {
+        Alert.alert(
+          'Remover',
+          'Deseja remover o grupo?',
+          [
+            { text: 'Não', style: 'cancel' },
+            { text: 'Sim', onPress: () => groupRemove() }
+          ]
+        )
       }
 
       useEffect(() => {
@@ -152,6 +177,7 @@ export function Players() {
             <Button 
                 title="Remover turma"
                 type="SECONDARY"
+                onPress={handleGroupRemove}
             />
         </Container>
     )
